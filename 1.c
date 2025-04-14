2459893f2e88dcec48ebb464da41cba62551b4b4
ahsoG08 <iskanderg47@gmail.com>
2025-04-14 22:42:54 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int main() {
-  int red_balls[10];
+int main() 
+{
+  int red_balls[10] = {1,2,3,4,5};
   int size = sizeof(red_balls) / sizeof(red_balls[0]);
   printf("В коробке red_balls помещается %d шариков.\n", size);
   return 0;
